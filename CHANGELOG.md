c5ca36db3d2a97ed3d03e1684137c75fced84010
Nathan Faubion <nathan@n-son.com>
2014-01-16 22:04:59 -0600

    Version bump (0.1.1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (2014-1-16)
+
+*   Changed positional constructors to use array indexing
+
 ## 0.1.0 (2014-1-10)
 
 *   Initial release
--- adt-simple.js
+++ adt-simple.js
@@ -2,7 +2,7 @@
  * adt-simple
  * ----------
  * author: Nathan Faubion <nathan@n-son.com>
- * version: 0.1.0
+ * version: 0.1.1
  * license: MIT
  */
 
